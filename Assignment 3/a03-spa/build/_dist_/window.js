628690565f1b63964ba6b5b3673510ace256ca9f
RonaZong <ronazong@gmail.com>
2023-02-10 22:03:26 +0100

    a3 v0

---
+++ Assignment 3/a03-spa/build/_dist_/window.js
@@ -0,0 +1,44 @@
+export default class window {
+  constructor (container) {
+    this.container = document.querySelector(container)
+    this.windowDiv = document.querySelectorAll(".windowContainer template")[0].content.firstElementChild
+    this.div = document.importNode(this.windowDiv, true)
+    this.container.appendChild(this.div)
+    this.closeWindowElement = this.div.childNodes[1].childNodes[1]
+    this.div.addEventListener("mousedown", this.mouseDown, true)
+    this.div.addEventListener("mouseup", this.mouseUp, true)
+    this.div.addEventListener("mousemove", this.mouseMove, true)
+    this.closeWindowElement.addEventListener('click', (event) => {
+      this.div.remove()
+    })
+  }
+
+  mouseDown (event) {
+    document.querySelectorAll(".window").forEach((window) => {
+      window.style.zIndex = -1
+    })
+    this.style.zIndex = 999
+    this.isDown = true
+    this.coordinates = [this.offsetLeft - event.clientX - 150,
+      this.offsetTop - event.clientY - 40]
+  }
+
+  mouseUp () {
+    this.isDown = false
+  }
+
+  /**
+   * on mouse down and move => update the corrdinates for the div
+   * @param event
+   */
+  mouseMove (event) {
+    if (this.isDown) {
+      this.mousePosition = {
+        x: event.clientX,
+        y: event.clientY
+      }
+      this.style.left = (this.mousePosition.x + this.coordinates[0]) + "px"
+      this.style.top = (this.mousePosition.y + this.coordinates[1]) + "px"
+    }
+  }
+}
